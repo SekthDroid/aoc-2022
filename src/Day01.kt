830dd6b7c783759b4a7172e6f71b66e0554019b3
David Sastre <david@monkeytaps.app>
2022-12-02 07:14:09 +0100

    Add day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,30 @@
 fun main() {
+    fun parseCarriers(input: List<String>): List<List<Int>> {
+        var current = mutableListOf<Int>()
+        var carriers = emptyList<List<Int>>()
+
+        for (each in input) {
+            if (each.isBlank()) {
+                carriers = carriers.plusElement(current)
+                current = mutableListOf()
+            } else {
+                current.add(each.toInt())
+            }
+        }
+        return carriers
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return parseCarriers(input).maxOf { it.sum() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return parseCarriers(input).map { it.sum() }.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
